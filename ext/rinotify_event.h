612fd7f930d71d33d5c40406eede97c7dabdd9ce
migrob <migrob@812c6237-2f46-4aec-9430-293760fcc8a5>
2007-03-31 20:19:56 +0000

    updated rinotify_event.c documentation
    git-svn-id: http://rinotify.rubyforge.org/svn@32 812c6237-2f46-4aec-9430-293760fcc8a5

--- ext/rinotify_event.h
+++ ext/rinotify_event.h
@@ -15,46 +15,45 @@ VALUE rb_cRInotifyEvent;
 
 /* Ruby Prototypes */
 
-/*
- *
- * call-seq:
- * 		RInotifyEvent.new => RInotifyEvent
- *
- * 	Returns a new RInotifyEvent object.  This method should only be called by RInotify.each_event
- *
- */
+// returns a new RInotifyEvent object.  This method should only be called by RInotify.each_event
 VALUE rb_rinotify_event_new(struct inotify_event*);
 
 
 /*
- *
  * call-seq:
- * 		RInotifyEvent.name => event->name
+ *	RInotifyEvent.name -> event name
  *
- * Returns the name of the event.
+ *	Returns the name of the event if the watched file is a directory.  If it is a file
+ *	nil is returned.  In this case the event name can be found by looking the watch descriptor
+ *	up using RInotify.watch_descriptors.
  *
+ *	Example Usage:
+ *	rinotify_event.name -> "/home/rob/desktop/tmp/test"
  */
 VALUE rb_rinotify_event_name(VALUE);
 
 
 /*
- *
  * call-seq:
- * 		RInotifyEvent.watch_descriptor => event->wd
+ *	RInotifyEvent.watch_descriptor -> event watch descriptor
  *
- * Returns the watch descriptor that the event belongs to.
+ *	Returns the watch descriptor that the event belongs to.
  *
+ *	Example Usage:
+ *	rinotify_event.watch_descriptor -> 1
  */
 VALUE rb_rinotify_event_watch_descriptor(VALUE);
 
 
 /*
- *
  * call-seq:
- * 		RInotifyEvent.check_mask => boolean
+ *	RInotifyEvent.check_mask(mask) -> boolean
  *
- * Returns true or false if the current event contains the mask(s)
+ *	Returns true or false if the current event contains the mask(s)
  *
+ *	Example Usage:
+ *	rinotify_event->check_mask(RInotify::MODIFY) -> true
+ *	rinotify_event->check_mask(RInotify::DELETE_SELF) -> false
  */
 VALUE rb_rinotify_event_check_mask(VALUE, VALUE);
 
